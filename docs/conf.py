8f9e7b297b10ca10641628fe3c12b750df67b820
Joshua Hernstrom <joshua.hernstrom@ni.com>
2022-07-07 19:37:56 -0500

    Add conf.py for rtd

---
+++ docs/conf.py
@@ -0,0 +1,88 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+#import recommonmark
+#from recommonmark.transform import AutoStructify
+import myst_parser
+
+source_suffix = ['.rst', '.md']
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'NI Automotive Camera Module Documentation'
+copyright = '2022, NI'
+author = 'NI'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = '1.0'
+# The full version, including alpha/beta/rc tagss
+release = '1.0.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+# extensions = [
+#    'recommonmark',
+# ]
+#extensions = ["myst_parser"]
+extensions = ["myst_nb"]
+
+    #  'sphinx.ext.autodoc',
+    #  'sphinx.ext.napoleon',
+    #  'sphinx.ext.mathjax',
+
+# Add any paths that contain templates here, relative to this directory.
+# templates_path = ['_templates']
+
+# The encoding of source files.
+#source_encoding = 'utf-8-sig'
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+# exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# app setup hook
+#def setup(app):
+#    app.add_config_value('recommonmark_config', {
+#        'auto_toc_maxdepth': 4,
+#        'enable_eval_rst': True,
+#    }, True)
+#    app.add_transform(AutoStructify)
+    
